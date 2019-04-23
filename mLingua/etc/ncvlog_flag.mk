187f36c472332f5cf0c90cb2474d594c6c8b39ec
Steven Herbst <sherbst@stanford.edu>
2019-04-23 16:04:56 -0700

    updates to make it easier to use makefile template

--- mLingua/etc/ncvlog_flag.mk
+++ mLingua/etc/ncvlog_flag.mk
@@ -1,6 +1,20 @@
-# Note that 'mLINGUA_DIR' is already set up in setup.cshrc
+# assumes that mLINGUA_DIR has already been set
+
 mLINGUA_INC_DIR := $(mLINGUA_DIR)/samples
 mLINGUA_VLOG_LIB_DIR := -y $(mLINGUA_DIR)/samples/prim -y $(mLINGUA_DIR)/samples/stim -y $(mLINGUA_DIR)/samples/meas -y $(mLINGUA_DIR)/samples/misc
 mLINGUA_PLI_DIR := $(mLINGUA_DIR)/samples/pli
 
+# generate main flags for ncvlog
+
 mLINGUA_NCVLOG_FLAGS := -y . $(mLINGUA_VLOG_LIB_DIR) +incdir+$(mLINGUA_INC_DIR) -sv +libext+.v+.vp +libext+.sv +define+NCVLOG -loadpli1 ${mLINGUA_PLI_DIR}/libpli.so:dave_boot -sem2009 -seed random +nc64bit
+
+# append extra arguments as desired
+
+ifneq ($(strip $(TOP_MODULE)),)
+	mLINGUA_NCVLOG_FLAGS += -top $(TOP_MODULE)
+endif
+
+ifneq ($(strip $(TIME_SCALE)),)
+	mLINGUA_NCVLOG_FLAGS += +nctimescale+$(TIME_SCALE)
+endif
+
--- mLingua/etc/vcs_flag.mk
+++ mLingua/etc/vcs_flag.mk
@@ -1,6 +1,9 @@
-# Note that 'mLINGUA_DIR' is already set up in setup.cshrc
+# assumes that mLINGUA_DIR is already defined
 mLINGUA_INC_DIR := $(mLINGUA_DIR)/samples
 mLINGUA_VLOG_LIB_DIR := -y $(mLINGUA_DIR)/samples/prim -y $(mLINGUA_DIR)/samples/stim -y $(mLINGUA_DIR)/samples/meas -y $(mLINGUA_DIR)/samples/misc
 mLINGUA_PLI_DIR := $(mLINGUA_DIR)/samples/pli
 
+# generate main flags for VCS
 mLINGUA_VCS_FLAGS := -y . $(mLINGUA_VLOG_LIB_DIR) +incdir+$(mLINGUA_INC_DIR) -sverilog +cli +lint=PCWM +libext+.v+.vp +libext+.sv -notice +v2k -debug_pp +define+VCS+VCS_MSG_QUIET -full64 -P $(mLINGUA_PLI_DIR)/pli_get_timeunit.tab $(mLINGUA_PLI_DIR)/pli_get_timeunit.so -CFLAGS "-g -I$VCS_HOME/`vcs -platform`/lib" +acc+3 
+# generate random seed used by VCS
+VCS_RND_SEED := $(strip $(shell head -4 /dev/urandom |od -N 4 -D -A n| awk '{print $1}')) # random seed generation
